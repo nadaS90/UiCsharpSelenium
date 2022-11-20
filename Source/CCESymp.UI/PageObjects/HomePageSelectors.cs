49439eb7b3e2eeb22eecd58820d38257fed07f8e
nadaS90 <engnada_45@outlook.com>
2022-11-20 18:40:30 +0200

    Add hoverTab option

---
+++ Source/CCESymp.UI/PageObjects/HomePageSelectors.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CCESymp.UI.PageObjects
+{
+    public partial class HomePage
+    {
+        private class Selectors
+        {
+            public static string EmailInputXpath => "//input[@id='email']";
+        }
+    }
--- Source/CCESymp.UI/Utilities/BasePage.cs
+++ Source/CCESymp.UI/Utilities/BasePage.cs
@@ -1,4 +1,5 @@
-﻿namespace CCESymp.UI.Utilities
+﻿using OpenQA.Selenium.Interactions;
+namespace CCESymp.UI.Utilities
 {
     public class BasePage<TypeOfPage> : BaseDriver where TypeOfPage : new()
     {
@@ -28,9 +29,18 @@
         public void RefreshPage()
         {
             driver.Navigation.Refresh();
-        }  
-        
+        }
 
-        
+        /// <summary>
+        /// Hover on tabs in current page
+        /// </summary>
+        public void HoverTab(IElement mainMenu, IElement subMenu)
+        {
+            Actions actions = new Actions(driver);
+            actions.moveToElement(element).perform();
+            actions.moveToElement(subMenu).click();
+           // actions.click().build().perform();
+        }
+
     }
 }
